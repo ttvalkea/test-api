73ea56a61281d974e0518df6506a61b02d40be18
Tuomas Valkeapää <tuomas.valkeapaa@codento.com>
2020-01-02 13:45:14 +0200

    Insignificant change

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -24,7 +24,7 @@ namespace TestApi.Controllers
                 {
                     new Item()
                     {
-                        Name = "Axe", Damage = rng.Next(5, 20), Defence = 0
+                        Name = "Axe", Damage = rng.Next(35, 50), Defence = 0
                     },
                     new Item()
                     {
